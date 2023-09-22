1d1e463ff0e8907f4ab83e3cc8fc0ba4d66e3d2e
sammidev87 <sammidev87@gmail.com>
2023-09-22 18:45:58 -0500

    updating for ajax and javascript

---
+++ David-Chua's-Restaurant/js/script.js
@@ -0,0 +1,33 @@
+$(function () { // $ is same as document.addEventListener("DOMContentLoaded")
+    $("#navbarToggle").blur(function (event) { // $ is same as document.querySelector("#navbarToggle")
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#collapsable-nav").collapse('hide');
+        }
+    });
+});
+
+(function (global) {
+    var dc = {};
+    var homeHtml = "snippets/home-snippet.html";
+
+    var insertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    var showLoading = function (selector) {
+        var html = "<div class='text-center'>";
+        html += "img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+    document.addEventListener("DOMContentLoaded", function (event) {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(homeHtml, function (responseText) {
+            document.querySelector("#main-content").innerHTML = responseText;
+        }, false);
+    });
+
+    global.$dc = dc;
+})(window);
