9eb9b9a522f226fda78f5acb689fd71e9f775bc1
sammidev87 <sammidev87@gmail.com>
2023-09-22 22:26:37 -0500

    finalized webpage

--- David-Chua's-Restaurant/js/script.js
+++ David-Chua's-Restaurant/js/script.js
@@ -13,6 +13,9 @@ $(function () { // $ is same as document.addEventListener("DOMContentLoaded")
     var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
+    var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+    var menuItemsTitleHtml = "snippets/menu-items-title.html";
+    var menuItemHtml = "snippets/menu-item.html";
 
     var insertHtml = function (selector, html) {
         var targetElem = document.querySelector(selector);
@@ -31,6 +34,18 @@ $(function () { // $ is same as document.addEventListener("DOMContentLoaded")
         return string;
     }
 
+    var switchMenuToActive = function () {
+        var classes = document.querySelector("#navHomeButton").className;
+        classes = classes.replace(new RegExp("active", "g"), "");
+        document.querySelector("#navHomeButton").className = classes;
+
+        classes = document.querySelector("#navMenuButton").className;
+        if (classes.indexOf("active") == -1) {
+            classes += " active";
+            document.querySelector("#navMenuButton").className = classes;
+        }
+    };
+
     document.addEventListener("DOMContentLoaded", function (event) {
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(homeHtml, function (responseText) {
@@ -43,9 +58,15 @@ $(function () { // $ is same as document.addEventListener("DOMContentLoaded")
         $ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
     };
 
-    function buildAndShowCategoriesHTML (categories) {
+    dc.loadMenuItems = function (categoryShort) {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(menuItemsUrl + categoryShort + ".json", buildAndShowMenuItemsHTML);
+    };
+
+    function buildAndShowCategoriesHTML(categories) {
         $ajaxUtils.sendGetRequest(categoriesTitleHtml, function (categoriesTitleHtml) {
             $ajaxUtils.sendGetRequest(categoryHtml, function (categoryHtml) {
+                switchMenuToActive();
                 var categoriesViewHtml = buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml);
                 insertHtml("#main-content", categoriesViewHtml);
             }, false);
@@ -58,8 +79,8 @@ $(function () { // $ is same as document.addEventListener("DOMContentLoaded")
 
         for (var i = 0; i < categories.length; i++) {
             var html = categoryHtml;
-            var name = "" + categories[i].name;
-            var short_name = categories[i].short_name;
+            var name = "" + categories[ i ].name;
+            var short_name = categories[ i ].short_name;
             html = insertProperty(html, "name", name);
             html = insertProperty(html, "short_name", short_name);
             finalHtml += html;
@@ -69,5 +90,63 @@ $(function () { // $ is same as document.addEventListener("DOMContentLoaded")
         return finalHtml;
     }
 
+    function buildAndShowMenuItemsHTML(categoryMenuItems) {
+        $ajaxUtils.sendGetRequest(menuItemsTitleHtml, function (menuItemsTitleHtml) {
+            $ajaxUtils.sendGetRequest(menuItemHtml, function (menuItemHtml) {
+                var menuItemsViewHtml = buildMenuItemsViewHtml(categoryMenuItems, menuItemsTitleHtml, menuItemHtml);
+                insertHtml("#main-content", menuItemsViewHtml);
+            }, false);
+        }, false);
+    }
+
+    function buildMenuItemsViewHtml(categoryMenuItems, menuItemsTitleHtml, menuItemHtml) {
+        menuItemsTitleHtml = insertProperty(menuItemsTitleHtml, "name", categoryMenuItems.category.name);
+        menuItemsTitleHtml = insertProperty(menuItemsTitleHtml, "special_instructions", categoryMenuItems.category.special_instructions);
+        var finalHtml = menuItemsTitleHtml;
+        finalHtml += "<section class='row'";
+
+        var menuItems = categoryMenuItems.menu_items;
+        var catShortName = categoryMenuItems.category.short_name;
+        for (var i = 0; i < menuItems.length; i++) {
+            var html = menuItemHtml;
+            html = insertProperty(html, "short_name", menuItems[ i ].short_name);
+            html = insertProperty(html, "catShortName", catShortName);
+            html = insertItemPrice(html, "price_small", menuItems[ i ].price_small);
+            html = insertItemPortionName(html, "small_portion_name", menuItems[ i ].small_portion_name);
+            html = insertItemPrice(html, "price_large", menuItems[ i ].price_large);
+            html = insertItemPortionName(html, "large_portion_name", menuItems[ i ].large_portion_name);
+            html = insertProperty(html, "name", menuItems[ i ].name);
+            html = insertProperty(html, "description", menuItems[ i ].description);
+            // Add clearfix after every second menu item
+            if (i % 2 != 0) {
+                html += "<div class='clearfix visible-lg-block visible-md-block'></div>";
+            }
+            finalHtml += html;
+        }
+
+        finalHtml += "</section>";
+        return finalHtml;
+    }
+
+    function insertItemPrice(html, pricePropName, priceValue) {
+        if (!priceValue) {
+            return insertProperty(html, pricePropName, "");
+        }
+
+        priceValue = "$" + priceValue.toFixed(2);
+        html = insertProperty(html, pricePropName, priceValue);
+        return html;
+    }
+
+    function insertItemPortionName(html, portionPropName, portionValue) {
+        if (!portionValue) {
+            return insertProperty(html, portionPropName, "");
+        }
+
+        portionValue = "(" + portionValue + ")";
+        html = insertProperty(html, portionPropName, portionValue);
+        return html;
+    }
+
     global.$dc = dc;
-})(window);
+})(window); htmlhtml
